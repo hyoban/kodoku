469600615f344744ca116eeb8ec0c366347340c5
hyoban <hi@hyoban.cc>
2023-04-22 23:59:09 +0800

    fix: height on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,12 +36,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <head />
         <body
           className={cn(
-            "min-h-screen bg-background font-sans antialiased",
+            "h-full bg-background font-sans antialiased",
             fontSans.variable
           )}
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
+            <div className="relative flex h-full flex-col">
               <SiteHeader />
               <div className="flex-1">{children}</div>
             </div>
