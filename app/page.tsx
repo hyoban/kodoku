a3dcc679feea156d9f9bf87780e47829867799fa
hyoban <hi@hyoban.cc>
2023-04-22 20:58:28 +0800

    chore: update site config

--- app/page.tsx
+++ app/page.tsx
@@ -6,32 +6,29 @@ import { buttonVariants } from "@/components/ui/button"
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
+      <div className="flex max-w-[900px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+          {siteConfig.description}
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
+          Based on the Incremental Static Regeneration function of Next.js and
+          shadcn/ui
         </p>
       </div>
       <div className="flex gap-4">
         <Link
-          href={siteConfig.links.docs}
+          href={siteConfig.links.github}
           target="_blank"
           rel="noreferrer"
           className={buttonVariants({ size: "lg" })}
         >
-          Documentation
+          Deploy Now
         </Link>
         <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
+          href="/"
           className={buttonVariants({ variant: "outline", size: "lg" })}
         >
-          GitHub
+          Demo
         </Link>
       </div>
     </section>
--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  name: "Kodoku",
+  description: "A website that helps you track RSS subscription updates",
   mainNav: [
     {
       title: "Home",
@@ -11,8 +10,7 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "https://twitter.com/0xhyoban",
+    github: "https://github.com/hyoban/kodoku",
   },
 }
