70ffe73dafbbc7af33a2c651066deae661a42df9
hyoban <hi@hyoban.cc>
2023-04-22 23:52:59 +0800

    fix: header on mobile

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,14 +13,12 @@ interface MainNavProps {
 export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="hidden items-center space-x-2 md:flex">
+      <Link href="/" className="flex items-center space-x-2">
         <Icons.logo className="h-6 w-6" />
-        <span className="hidden font-bold sm:inline-block">
-          {siteConfig.name}
-        </span>
+        <span className="inline-block font-bold">{siteConfig.name}</span>
       </Link>
       {items?.length ? (
-        <nav className="hidden gap-6 md:flex">
+        <nav className="flex gap-6">
           {items?.map(
             (item, index) =>
               item.href && (
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -11,7 +11,7 @@ export function SiteHeader() {
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
-        <div className="flex flex-1 items-center justify-end space-x-4">
+        <div className="hidden flex-1 items-center justify-end space-x-4 sm:flex">
           <nav className="flex items-center space-x-1">
             <Link
               href={siteConfig.links.github}
