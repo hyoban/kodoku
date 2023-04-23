78fdc4b11e92d49aeb09b1ca23f0327bc028d5ea
hyoban <hi@hyoban.cc>
2023-04-23 23:44:29 +0800

    refactor: create feed list group component

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -3,16 +3,9 @@ import dayjs from "dayjs"
 
 import { siteConfig } from "@/config/site"
 import { getFeedList } from "@/lib/notion"
-import { capitalize, firstSentence } from "@/lib/utils"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import { capitalize } from "@/lib/utils"
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import FeedListGroup from "@/components/feed-list-group"
 
 const { timeZone } = siteConfig
 
@@ -24,12 +17,11 @@ export default async function SubscriptionPage() {
     return null
   }
 
-  const languageSet = ["all"].concat(
-    Array.from(new Set(feedList.map((feed) => feed.language.toLowerCase())))
-  )
-
   const typeSet = ["all"].concat(
     Array.from(new Set(feedList.map((feed) => feed.type.toLowerCase())))
+  )
+  const languageSet = ["all"].concat(
+    Array.from(new Set(feedList.map((feed) => feed.language.toLowerCase())))
   )
 
   const feedListGroupedByYearAndMonth = feedList.reduce((acc, feed) => {
@@ -46,84 +38,31 @@ export default async function SubscriptionPage() {
       <div className="container my-14 w-full max-w-5xl">
         <Tabs defaultValue={"all"} className="mb-4">
           <TabsList>
-            {languageSet.map((language) => (
-              <TabsTrigger key={language} value={language}>
-                {capitalize(language)}
-              </TabsTrigger>
-            ))}
-          </TabsList>
-        </Tabs>
-
-        <Tabs defaultValue={"all"}>
-          <TabsList>
             {typeSet.map((type) => (
               <TabsTrigger key={type} value={type}>
-                {capitalize(type)}
+                <Link href={`/subscription/${type}/all`}>
+                  {capitalize(type)}
+                </Link>
               </TabsTrigger>
             ))}
           </TabsList>
         </Tabs>
 
-        {Object.keys(feedListGroupedByYearAndMonth)
-          .sort((a, b) => Number(b) - Number(a))
-          .map((feedMonth) => {
-            const feedListByYear = feedListGroupedByYearAndMonth[feedMonth]
-            return (
-              <div className="my-10" key={feedMonth}>
-                <h2 className="my-4 text-2xl font-bold">{feedMonth}</h2>
-                <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-                  {feedListByYear.map((feed) => (
-                    <Card
-                      className="h-full transition-transform hover:scale-105"
-                      key={feed.link}
-                    >
-                      <CardHeader>
-                        <CardTitle>{feed.title}</CardTitle>
-                        <CardDescription>
-                          <Link
-                            href={feed.homeUrl}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            {feed.author}
-                          </Link>
-                        </CardDescription>
-                        <CardDescription>
-                          {dayjs(feed.isoDate)
-                            .tz(timeZone)
-                            .format("MM-DD HH:mm")}
-                        </CardDescription>
-                        <CardDescription>{feed.language}</CardDescription>
-                      </CardHeader>
-                      <CardContent>
-                        <Link
-                          href={feed.link}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="break-all"
-                        >
-                          {firstSentence(feed.contentSnippet ?? "").slice(
-                            0,
-                            100
-                          )}
-                        </Link>
-                      </CardContent>
-                      <CardFooter className="flex flex-wrap gap-2">
-                        {feed.categories?.map((category) => (
-                          <span
-                            key={category}
-                            className="rounded bg-gray-600 px-2 py-1 text-xs text-white"
-                          >
-                            {category}
-                          </span>
-                        ))}
-                      </CardFooter>
-                    </Card>
-                  ))}
-                </div>
-              </div>
-            )
-          })}
+        <Tabs defaultValue={"all"} className="mb-4">
+          <TabsList>
+            {languageSet.map((language) => (
+              <TabsTrigger key={language} value={language}>
+                <Link href={`/subscription/all/${language}`}>
+                  {capitalize(language)}
+                </Link>
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </Tabs>
+
+        <FeedListGroup
+          feedListGroupedByYearAndMonth={feedListGroupedByYearAndMonth}
+        />
       </div>
     </>
   )
---
+++ components/feed-list-group.tsx
@@ -0,0 +1,82 @@
+import Link from "next/link"
+import dayjs from "dayjs"
+
+import { siteConfig } from "@/config/site"
+import { FeedList } from "@/lib/notion"
+import { firstSentence } from "@/lib/utils"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+const { timeZone } = siteConfig
+
+export default function FeedListGroup({
+  feedListGroupedByYearAndMonth,
+}: {
+  feedListGroupedByYearAndMonth: Record<string, FeedList>
+}) {
+  return (
+    <>
+      {Object.keys(feedListGroupedByYearAndMonth)
+        .sort((a, b) => Number(b) - Number(a))
+        .map((feedMonth) => {
+          const feedListByYear = feedListGroupedByYearAndMonth[feedMonth]
+          return (
+            <div className="my-10" key={feedMonth}>
+              <h2 className="my-4 text-2xl font-bold">{feedMonth}</h2>
+              <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+                {feedListByYear.map((feed) => (
+                  <Card
+                    className="h-full transition-transform hover:scale-105"
+                    key={feed.link}
+                  >
+                    <CardHeader>
+                      <CardTitle>{feed.title}</CardTitle>
+                      <CardDescription>
+                        <Link
+                          href={feed.homeUrl}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {feed.author}
+                        </Link>
+                      </CardDescription>
+                      <CardDescription>
+                        {dayjs(feed.isoDate).tz(timeZone).format("MM-DD HH:mm")}
+                      </CardDescription>
+                      <CardDescription>{feed.language}</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                      <Link
+                        href={feed.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="break-all"
+                      >
+                        {firstSentence(feed.contentSnippet ?? "").slice(0, 100)}
+                      </Link>
+                    </CardContent>
+                    <CardFooter className="flex flex-wrap gap-2">
+                      {feed.categories?.map((category) => (
+                        <span
+                          key={category}
+                          className="rounded bg-gray-600 px-2 py-1 text-xs text-white"
+                        >
+                          {category}
+                        </span>
+                      ))}
+                    </CardFooter>
+                  </Card>
+                ))}
+              </div>
+            </div>
+          )
+        })}
+    </>
+  )
+}
