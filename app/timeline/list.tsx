64f137022af9c63a0f5653db6724c360e0f0d3a1
Stephen Zhou <hi@hyoban.cc>
2023-05-14 02:38:50 +0800

    chore: update

--- app/timeline/list.tsx
+++ app/timeline/list.tsx
@@ -16,26 +16,26 @@ const { timeZone } = siteConfig
 
 export default function Timeline({ timeline }: { timeline: FeedList }) {
 	const [selectedType] = useAtom(selectedTypeAtom)
-
 	return (
 		<>
 			{timeline
 				.filter((item) => selectedType.includes(item.feedInfo.type))
 				.map((item) => {
 					return (
-						<div key={item.contentSnippet}>
-							<div className="flex items-center gap-4 py-4 md:gap-10">
-								<Image
-									src={item.feedInfo.avatar}
-									alt={item.feedInfo.title}
-									width={36}
-									height={36}
-									className="my-4 h-9 w-9 self-start rounded-full"
-								></Image>
-								<div className="space-y-3">
+						<div key={item.contentSnippet} className="my-4 space-y-4">
+							<div className="flex flex-col gap-4">
+								<div className="flex gap-4">
+									<Image
+										src={item.feedInfo.avatar}
+										alt={item.feedInfo.title}
+										width={36}
+										height={36}
+										className="h-9 w-9 rounded-full border"
+									></Image>
+
 									{item.feedInfo.type !== "GitHub" && (
-										<>
-											<h2 className="my-4 text-lg font-semibold">
+										<div>
+											<h2 className="text-lg font-semibold">
 												<a
 													href={item.link}
 													target="_blank"
@@ -53,24 +53,26 @@ export default function Timeline({ timeline }: { timeline: FeedList }) {
 											>
 												{dayjs(item.isoDate).tz(timeZone).fromNow()}
 											</p>
-										</>
+										</div>
 									)}
-
-									<div
-										className={cn(
-											"prose break-all dark:prose-invert [&>iframe]:hidden",
-											item.feedInfo.type === "GitHub"
-												? "prose-img:hidden [&>div>div>div>div>div:nth-child(2)>svg]:hidden"
-												: item.feedInfo.type === "Twitter"
-												? "prose-img:my-2 prose-img:max-w-[calc(100%-1rem)] prose-img:rounded-lg prose-img:border sm:prose-img:max-w-xs"
-												: "prose-img:rounded"
-										)}
-										dangerouslySetInnerHTML={{
-											__html: item.content?.replaceAll("<br>", " ") ?? "",
-										}}
-									></div>
-									<Badge variant={"outline"}>{item.feedInfo.type}</Badge>
 								</div>
+
+								<div
+									className={cn(
+										"prose break-all dark:prose-invert [&>iframe]:hidden",
+										item.feedInfo.type === "GitHub"
+											? "prose-img:hidden [&>div>div>div>div>div:nth-child(2)>svg]:hidden"
+											: item.feedInfo.type === "Twitter"
+											? "prose-img:my-2 prose-img:max-w-full prose-img:rounded-lg prose-img:border sm:prose-img:max-w-xs"
+											: "prose-img:rounded"
+									)}
+									dangerouslySetInnerHTML={{
+										__html: item.content?.replaceAll("<br>", " ") ?? "",
+									}}
+								></div>
+								<Badge variant={"outline"} className="w-fit">
+									{item.feedInfo.type}
+								</Badge>
 							</div>
 							<Separator orientation="horizontal"></Separator>
 						</div>
